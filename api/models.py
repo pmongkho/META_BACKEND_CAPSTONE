3d89365c257491955ada11e5cd286dfd219202e1
Phongsavanh Mongkhonvilay <azulatech@PhongsavanhsMBP.lan>
2023-01-20 22:25:36 -0600

    add tests

--- api/models.py
+++ api/models.py
@@ -3,10 +3,18 @@ from django.db import models
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=255)
+    table_no = models.IntegerField(default=0)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
     
+    
 class Menu(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_length=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+        
+    
+    
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from . import views
 
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-     
+     path('restaurant/booking/', include(router.urls)),
+     path('menu-items/', views.MenuItemView.as_view()),
+     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
  ]
  
